d7c2a332d9958eb8083fb75f06a25617f8fa3c20
Yorwos <46780934+yorwosa@users.noreply.github.com>
2019-01-29 15:23:12 +0200

    Finished Exercise 9_3
    Encryption Decryption Script

--- chapter_9/exercise9_3/exercise9_3.py
+++ chapter_9/exercise9_3/exercise9_3.py
@@ -1,28 +1,29 @@
 def encryption_rules():
     encryption_dataset = dict()
-    characters = "~`!@#$%^&*()_+-={[}]:;'?/>.<,1234567890abcdefghijklmnopqrstuvwxABCDE"
-    letters = '''AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz 1234567890'"-.,'''
+    decryption_dataset = dict()
+    characters = "!@#$%^&*()_+~`1234567890-={[}]:;>.<,?/AaBbCcFfRrDdTtGgYyHhJjUuKkOoLlp"
+    letters = """AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz 1234567890’'"-.,"""
     for index in range(len(letters)):
         encryption_dataset[letters[index]] = characters[index]
-    return encryption_dataset
-
+        decryption_dataset[characters[index]] = letters[index]
+    return encryption_dataset, decryption_dataset
 
 def main():
-    dataset = encryption_rules()
+    encryption_dataset, decryption_dataset = encryption_rules()
     filename = input('Select a txt file: ')
     print()
     choice = int(input('Press 1 to Encrypt, 2 to Decrypt: '))
     if choice == 1:
-        encrypt(filename,dataset)
+        encrypt(filename,encryption_dataset)
     elif choice == 2:
-        decrypt(filename,dataset)
+        decrypt(filename,decryption_dataset)
     else:
         print('Not a valid choice')
 
-def encrypt(a_file,encryption_rules):
+def encrypt(a_file,encryption_rule):
     # Open the infile and outfile.
-    infile = open(a_file,'r')
-    outfile = open('Encrypted_' + a_file,'w')
+    infile = open(a_file,'r', encoding='utf8')
+    outfile = open('encrypted_' + a_file,'w', encoding='utf8')
     # Strip the contents of '\n'
     text = []
     encrypted_text = []
@@ -35,12 +36,33 @@ def encrypt(a_file,encryption_rules):
         for char in line:
             # Find the character's value in the dictionary
             # and added to the encrypted text line.
-            encrypted_line += str(encryption_rules.get(char))
+            encrypted_line += str(encryption_rule.get(char))
         encrypted_text.append(encrypted_line)
+    for line in encrypted_text:
+        outfile.write(line + '\n')
+    infile.close()
+    outfile.close()
 
 def decrypt(a_file,decryption_rules):
-    
-   
-   
-
+    # Open the infile and outfile
+    infile = open('encrypted_' + a_file,'r', encoding='utf8')
+    outfile = open('decrypted_' + a_file,'w', encoding='utf8')
+    # Strip the contents of \n
+    encrypted_text = []
+    decrypted_text = []
+    for line in infile:
+        encrypted_text.append(line.rstrip('\n'))
+    # For every line in the text file.
+    for line in encrypted_text:
+        decrypted_line = ''
+        # For every character in each line.
+        for char in line:
+            # Find the character's value in the dictionary
+            # and add it to the decrypted text line
+            decrypted_line += str(decryption_rules.get(char))
+        decrypted_text.append(decrypted_line)
+    for line in decrypted_text:
+        outfile.write(line + '\n')
+    infile.close()
+    outfile.close()
 main()
